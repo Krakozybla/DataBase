7d9b259515fbf13bb06d8736a98a278fcb2d40a8
Krakozybla <lavrovwebdev1995@gmail.com>
2021-07-12 01:41:32 +0300

    added btn remove card

--- main.js
+++ main.js
@@ -24,23 +24,31 @@ let person = [
 let valInp = '';
 const inp = document.querySelector('.inp');
 const btn = document.querySelector('.btn');
+let resNeagitive = document.querySelector('.res-negative');
+let container;
+
 btn.addEventListener('click', ()=>{
     valInp = inp.value;
     let map = false;
+    if(valInp.length <= 0){
+        resNeagitive.textContent = "Write name of person you are looking now";
+        return '';
+    }
     person.forEach((item, index)=>{
         if(valInp === item.name){
             map = true;
             showPerson(item, index);
+            resNeagitive.textContent = "";
         }
     })
     if(!map){
-        console.log('Such person in not excist in DataBase');
+        resNeagitive.textContent = `Such person ${valInp} is not excist in DataBase`;
     }
     inp.value = '';
 })
 
 const showPerson = (item, index) =>{
-    const container = document.querySelector('.container');
+    container = document.querySelector('.container');
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
@@ -51,6 +59,15 @@ const showPerson = (item, index) =>{
     <li class="army">Army: ${item.army}</li>
     <li class="age">Age: ${item.age}</li>
     <li class="born">Born in: ${item.born}</li>
+    <button class="btn-del">Delite</button>
     </div>
     `  ) 
+
+    container.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('btn-del')){
+            const target = e.target.closest('.person-info')
+            target.remove();
+        }
+    })
 }
+
